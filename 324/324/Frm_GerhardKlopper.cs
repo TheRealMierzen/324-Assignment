cd9c101e9a46029514c0d1b1ced40240acbfec50
Ulargrind <ulargrind@gmail.com>
2017-08-07 14:42:58 +0200

    Remove integer checking.

--- 324/324/Frm_GerhardKlopper.cs
+++ 324/324/Frm_GerhardKlopper.cs
@@ -22,15 +22,18 @@ namespace _324
         private void btnCreate_Click(object sender, EventArgs e)
         {
             int n = 0;
-            if (int.TryParse(textBox1.Text, out n))
-            {
-                numberholder.AddNRandomNumbers(n, 0, 25);
-                lblNumbers.Text = numberholder.PrintNumbers();
-            }
-            else
-            {
-                MessageBox.Show("Not a number");
-            }
+            int.TryParse(textBox1.Text, out n); numberholder.AddNRandomNumbers(n, 0, 25);
+            lblNumbers.Text = numberholder.PrintNumbers();
+
+            //if (int.TryParse(textBox1.Text, out n))
+            //{
+            //    numberholder.AddNRandomNumbers(n, 0, 25);
+            //    lblNumbers.Text = numberholder.PrintNumbers();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Not a number");
+            //}
 
         }
 
